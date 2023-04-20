8d510306088f153adfadbd8349f3e4dcd02ded31
kanna <Sukhesh.K@skillassure.com>
2023-04-20 15:12:42 +0530

    Add project files.

---
+++ Controllers/ExternalDataController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DummyApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ExternalDataController : ControllerBase
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ExternalDataController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        [Route("api/Technology/technologies")]
+        public async Task<IActionResult> GetTechnologiesData()
+        {
+            string url = "https://auditapi-test.skillcheck.skillassure.com/api/Technology/technologies";
+            try
+            {
+                HttpClient httpClient = _httpClientFactory.CreateClient();
+                HttpResponseMessage response = await httpClient.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseData = await response.Content.ReadAsStringAsync();
+                    return Ok(responseData);
+                }
+                else
+                {
+                    throw new Exception("Failed to retrieve data.");
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"An error occurred while retrieving data. Error message: {ex.Message}");
+            }
+        }
+        [HttpGet]
+        public async Task<ActionResult<string>> Get()
+        {
+            try
+            {
+                var client = _httpClientFactory.CreateClient();
+                var response = await client.GetAsync("https://auditapi-test.skillcheck.skillassure.com/api/UserAuditcheck/userAuditCheck");
+                response.EnsureSuccessStatusCode();
+                var responseBody = await response.Content.ReadAsStringAsync();
+                return Ok(responseBody);
+            }
+            catch (HttpRequestException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
---
+++ Model/UserAuditcheck.cs
@@ -0,0 +1,12 @@
+﻿namespace DummyApi.Model
+{
+    public class UserAuditcheck
+    {
+        public Guid userAuditCheckID { get; set; }
+        public bool isActive { get; set; }
+        public bool isUpdated { get; set; }
+        public string userProficiencyCode { get; set; }
+        public string userProficiencyName { get; set; }
+        public Guid auditCheckID { get; set; }
+    }
+}
